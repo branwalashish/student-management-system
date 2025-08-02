d3ae768db90dd8f68d2ff9be4768d1810b148f70
Ashish kumar <72202312+branwalashish@users.noreply.github.com>
2025-08-02 17:03:07 +0530

    Delete Day-5 directory

--- Day-5/script.js
+++
@@ -1,8 +0,0 @@
-function dark(){
-    document.body.style.backgroundColor="black";
-    document.body.style.color="white"
-}
-function light(){
-    document.body.style.backgroundColor="white";
-    document.body.style.color="black"
-}
